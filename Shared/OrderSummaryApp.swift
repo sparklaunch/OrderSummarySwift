cd4dae5f7f385160de0be07686b21971144aff5a
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-01-01 12:38:15 +0900

    Initial Commit

---
+++ Shared/OrderSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderSummaryApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct OrderSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
