3d443c67913a8e5569d5533b4ffd3f340adb7de1
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-01-01 12:45:30 +0900

    Initialization

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Common.Colors.kBackgroundColor
+            VStack {
+                Image("Hero")
+                    .resizable()
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius: 10)
+            .padding(.horizontal, 30)
+            .scaledToFit()
+        }
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
     }
 }
---
+++ Shared/Utilities/Common.swift
@@ -0,0 +1,14 @@
+//
+//  Common.swift
+//  OrderSummary (iOS)
+//
+//  Created by Jinwook Kim on 2022/01/01.
+//
+
+import SwiftUI
+
+struct Common {
+    struct Colors {
+        static let kBackgroundColor: Color = Color(red: 208 / 256, green: 220 / 256, blue: 255 / 256)
+    }
+}
